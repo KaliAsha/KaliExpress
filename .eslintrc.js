4b7bdbcd879745bc24a0a1068195855233ae216a
KaliAsha <kaliasha.kali@gmail.com>
2016-06-02 19:21:01 +0200

    Add ESLint and install packages

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  'extends': 'standard',
+  'plugins': [
+    'standard'
+  ],
+  'env': {
+    'node': true,
+    'es6': true
+  },
+  'rules': {
+    'no-multiple-empty-lines': 0,
+    'space-before-function-paren': 0,
+    'padded-blocks': 0
+  }
+}
