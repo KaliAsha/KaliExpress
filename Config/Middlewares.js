b346d1dc40b833321e7f39819be570385ba6f6dc
KaliAsha <kaliasha.kali@gmail.com>
2016-06-05 19:18:29 +0200

    Enable CORS

--- Config/Middlewares.js
+++ Config/Middlewares.js
@@ -3,11 +3,18 @@ const logger = require('morgan')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 
+function enableCORS (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+}
+
 const Middlewares = [
   logger('dev'),
   bodyParser.json(),
   bodyParser.urlencoded({ extended: false }),
-  cookieParser()
+  cookieParser(),
+  enableCORS
 ]
 
 module.exports = Middlewares
